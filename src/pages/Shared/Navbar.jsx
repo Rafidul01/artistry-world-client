60d3722fb4ce9e124b54166d4bd5199f80211304
Rafidul01 <rafidulhasan555@gmail.com>
2024-04-27 01:10:10 +0600

    dark mode added

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+    useEffect(()=>{
+        localStorage.setItem('theme', theme)
+        const localTheme = localStorage.getItem('theme')
+        document.querySelector('html').setAttribute('data-theme',localTheme)
+    },[theme])
+
+
+    const handleToggle = (e) =>{
+        if(e.target.checked){
+            setTheme('dark')
+        }
+        else setTheme('light')
+
+        console.log(e.target.checked);
+    }
+    
   const links = (
     <>
       <li className="hover:text-[#973E12]">
@@ -46,7 +66,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 p-0 my-4">
+    <div className="navbar bg-base-100 p-0 my-4 border-b-2 rounded-xl px-2 shadow-lg">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -87,16 +107,55 @@ const Navbar = () => {
       <div className="navbar-end font-lato">
         <Link
           to="/login"
-          className="btn bg-[#973E12]  text-white font-poppins rounded-lg min-h-0 h-10 md:min-h-[3rem] md:h-[3rem]"
+          className="btn bg-[#973E12]  text-white font-poppins rounded-3xl min-h-0 h-10 md:min-h-[3rem] md:h-[3rem]"
         >
           Login
         </Link>
         <Link
           to="/register"
-          className=" hidden md:flex ml-2 btn bg-[#973E12] text-white font-poppins rounded-lg min-h-0 h-10 md:min-h-[3rem] md:h-[3rem]"
+          className=" hidden md:flex ml-2 btn bg-[#973E12] text-white font-poppins rounded-3xl min-h-0 h-10 md:min-h-[3rem] md:h-[3rem]"
         >
           Register
         </Link>
+
+        {/* theme */}
+        <label className="cursor-pointer grid place-items-center ml-2 ">
+          <input
+            type="checkbox"
+            onChange={handleToggle}
+            checked={theme==='dark'? true : false}
+            className="toggle theme-controller bg-[#973E12] row-start-1 col-start-1 col-span-2"
+          />
+          <svg
+            className="col-start-1 row-start-1 stroke-base-100 fill-base-100 "
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
+          <svg
+            className="col-start-2 row-start-1 stroke-base-100 fill-base-100  "
+            xmlns="http://www.w3.org/2000/svg"
+            width="14"
+            height="14"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
       </div>
     </div>
   );
