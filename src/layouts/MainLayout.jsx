b1bfcebfcf53087e927eb940a1ea1defaf6a925f
Rafidul01 <rafidulhasan555@gmail.com>
2024-04-27 02:44:13 +0600

    register page added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../pages/Shared/Navbar";
+import { ToastContainer } from "react-toastify";
 
 const MainLayout = () => {
-    return (
-        <div className="container mx-auto">
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <div className="container mx-auto">
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default MainLayout;
