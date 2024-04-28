716d67fac9c313158adf8d1938a472e54cded876
Rafidul01 <rafidulhasan555@gmail.com>
2024-04-28 23:13:20 +0600

    customization filter added

--- src/pages/MyArtAndCraft/MyArtAndCraft.jsx
+++ src/pages/MyArtAndCraft/MyArtAndCraft.jsx
@@ -1,21 +1,52 @@
 import { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import MyArtCard from "./MyArtCard";
 
 const MyArtAndCraft = () => {
-    const loadedcraft = useLoaderData();
-    const [crafts, setCrafts] = useState(loadedcraft);
-    return (
-        <div>
-           
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-                {
-                    crafts.map(craft => <MyArtCard key={craft._id} craft={craft} setCrafts={setCrafts} crafts={crafts}></MyArtCard> )
-                } 
-            </div>
-            
-        </div>
-    );
+  const loadedcraft = useLoaderData();
+  const [crafts, setCrafts] = useState(loadedcraft);
+  const params = useParams();
+  const {email} = params;
+//   console.log(email);
+  const handleFilter = e =>{
+    console.log(e.target.value);
+    const option = e.target.value;
+    if(option === "Any"){
+        setCrafts(loadedcraft);
+        return;
+    }
+    fetch(`http://localhost:5000/myCrafts/${email}/${option}`)
+    .then(res => res.json())
+    .then(data => {
+        setCrafts(data);
+    })
+  }
+
+  return (
+    <div className="">
+      <div className="flex justify-center items-center pt-4 pb-8">
+        <select onChange={handleFilter} className="select select-primary w-full max-w-xs border-[#973e12] focus:border-[#973e12]  focus:outline-[#973e12] font-bold text-center text-[#973e12]">
+          <option disabled selected>
+          Customization
+          </option>
+          <option >Any</option>
+          <option>Yes</option>
+          <option>No</option>
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+        {crafts.map((craft) => (
+          <MyArtCard
+            key={craft._id}
+            craft={craft}
+            setCrafts={setCrafts}
+            crafts={crafts}
+          ></MyArtCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyArtAndCraft;
